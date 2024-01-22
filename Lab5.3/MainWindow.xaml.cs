957db518454f1c62c24a17b2b7b6afcb974cdd4a
TheSkyEye1 <golovan4ickoff@gmail.com>
2024-01-22 12:12:08 +0700

    Добавьте файлы проекта.

---
+++ Lab5.3/MainWindow.xaml.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Lab5._3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<MovingObject> Objects = new List<MovingObject>();
+        public List<Point> Points = new List<Point>();
+
+        Random rnd;
+
+        int pointCount = 5;
+        int movingObjCount = 10;
+        Point startPos = new Point(250,250);
+        int startRotation = 0;
+
+        int pointR = 10;
+        int objR = 15;
+
+        int iteration = 0;
+        int maxiterations = 1000;
+        DispatcherTimer timer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            rnd = new Random();
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 100);
+            timer.Tick += Timer_Tick;
+        }
+
+        public void CreatePoints()
+        {
+            Points.Clear();
+            for(int i = 0; i<pointCount; i++)
+            {
+                Points.Add(new Point(rnd.Next(9,490), rnd.Next(9, 490)));
+            }
+        }
+
+        public void initPopulation()
+        {
+            for(int i = 0; i<movingObjCount; i++)
+            {
+                Objects.Add(new MovingObject(startPos, startRotation, rnd));
+            }
+        }
+
+        public void drawEllipse(Point p, double r, int type)
+        {
+            Ellipse el = new Ellipse();
+            SolidColorBrush cb = new SolidColorBrush();
+            if (type == 0) cb.Color = Color.FromArgb(255, 0, 0, 0);
+            else cb.Color = Color.FromArgb(0, 255, 0, 0);
+            el.Fill = cb;
+            el.StrokeThickness = 1;
+            el.Stroke = Brushes.Black;
+            el.Width = r;
+            el.Height = r;
+            el.RenderTransform = new TranslateTransform(p.X - r/2, p.Y - r / 2);
+            scene.Children.Add(el);
+        }
+
+        public void DrawScene()
+        {
+            scene.Children.Clear();
+            foreach (Point p in Points)
+            {
+                drawEllipse(p, pointR, 1);
+            }
+            foreach (MovingObject obj in Objects)
+            {
+                drawEllipse(obj.position, objR, 0);
+            }
+        }
+
+        private void StartB_Click(object sender, RoutedEventArgs e)
+        {
+            CreatePoints();
+            DrawScene();
+            timer.Start();
+            
+        }
+
+        private void InitPopB_Click(object sender, RoutedEventArgs e)
+        {
+            initPopulation();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if(iteration < maxiterations)
+            {
+                itcounter.Content = iteration;
+                foreach(MovingObject obj in Objects)
+                {
+                    obj.Moves[obj.movecounter]();
+                    obj.movecounter++;
+                    if (obj.movecounter == obj.Moves.Count) obj.movecounter = 0;
+                }
+                DrawScene();
+                iteration++;
+            }
+            else
+            {
+                iteration = 0;
+                timer.Stop();
+            }
+        }
+    }
+}
---
+++ Lab5.3/MovingObject.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using static Lab5._3.MovingObject;
+
+namespace Lab5._3
+{
+    public class MovingObject
+    {
+        Random rnd;
+
+        public Point position;
+        public double currentRotation;
+        public int fitness = 0;
+        public int movecounter = 0;
+
+        int distance = 15;
+
+        public List<Move> Moves = new List<Move>();
+
+        public delegate void Move();
+
+        
+        public void MoveForward()
+        {
+            double x = position.X + distance * Math.Cos(currentRotation);
+            double y = position.Y + distance * Math.Sin(currentRotation);
+            if (x > 500) x = 484;
+            if (x < 0) x = 14;
+            if (y > 500) y = 484;
+            if (y < 0) y = 14;
+
+            position = new Point(x, y);
+        }
+
+        public void RotateLeft()
+        {
+            if (currentRotation == 0) currentRotation = 315;
+            else currentRotation -= 45;
+        }
+
+        public void RotateRight()
+        {
+            if (currentRotation == 315) currentRotation = 0;
+            else currentRotation += 45;
+        }
+        public List<Move> CreateGenes()
+        {
+            List<Move> movlist = new List<Move>();
+            int movescount = rnd.Next(25, 50);
+            for(int i = 0; i<movescount; i++)
+            {
+                int m = rnd.Next(3);
+                switch(m)
+                {
+                    case 0:
+                        Move moveforward = MoveForward;
+                        movlist.Add(moveforward);
+                        break;
+                    case 1:
+                        Move moveleft = RotateLeft;
+                        movlist.Add(moveleft);
+                        break;
+                    case 2:
+                        Move moveright = RotateRight;
+                        movlist.Add(moveright);
+                        break;
+                }
+            }
+            return movlist;
+        }
+
+        public void ResetPos(Point pos, double curRot)
+        {
+            this.position = pos;
+            this.currentRotation = curRot;
+        }
+
+        public MovingObject(Point position, double currentRotation, Random rnd)
+        {
+            this.rnd = rnd;
+            this.position = position;
+            this.currentRotation = 0;
+            this.Moves = CreateGenes();
+            
+        }
+    }
+}
